412e69c6c6f841cbf07a8eb167e63fc2f462750e
davibelini <dev.dado.belini@gmail.com>
2020-11-21 21:18:00 -0300

    window setup

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import pygame
+import os
+import time
+import random
+
+# setup
+width = 850
+height = 700
+col_grey = (95, 96, 95)
+window = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Jet Fighter")
+# game
+def main():
+  run = True
+  fps = 60
+  clock = pygame.time.Clock()
+  def redraw_window():
+    window.fill(col_grey)
+    pygame.display.update()
+  while run:
+    clock.tick(fps)
+    redraw_window()
+    for e in pygame.event.get():
+      if e.type == pygame.QUIT:
+            run = False
+main()

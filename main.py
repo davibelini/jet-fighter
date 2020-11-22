45b16d1734ee1fc38fd56658d60a453b4568bb42
davibelini <dev.dado.belini@gmail.com>
2020-11-21 22:51:55 -0300

    loaded 'black_ship' image

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ height = 700
 col_grey = (95, 96, 95)
 window = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Jet Fighter")
+###
+# import sprites
+black_ship = pygame.image.load(os.path.join("assets", "black_ship.png"))
+###
 # game
 def main():
   run = True
   fps = 60
   clock = pygame.time.Clock()
-  def redraw_window():
+  def redraw():
     window.fill(col_grey)
     pygame.display.update()
   while run:
     clock.tick(fps)
-    redraw_window()
+    redraw()
     for e in pygame.event.get():
       if e.type == pygame.QUIT:
             run = False

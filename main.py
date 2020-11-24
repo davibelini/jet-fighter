aa3aa79c44bf0ed1ad078fe42818dfc301b3be61
davibelini <dev.dado.belini@gmail.com>
2020-11-24 15:46:21 -0300

    modified main

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ left_start_coord = (100, 300)
 white_ship = pygame.transform.scale(pygame.image.load(os.path.join("assets", "white_ship.png")), (32, 32))
 right_start_coord = (width - 150, 300)
 ###
+# data
+black_ship_angle = 0
+###
 # game
 def main():
+  global black_ship_angle
   run = True
   fps = 60
   clock = pygame.time.Clock()
@@ -31,5 +35,10 @@ def main():
     redraw()
     for e in pygame.event.get():
       if e.type == pygame.QUIT:
-            run = False
+        run = False
+      if e.type == pygame.KEYDOWN:
+        if e.key == pygame.K_LEFT:
+          print("pressed k_left")
+          # pygame.transform.rotate(black_ship, black_ship_angle) # it's not working
+          black_ship_angle += 1
 main()
